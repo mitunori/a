9ac5fdc9a21f49d11e55f267680b7da47d145463
mitsunori-morita <mitsunori.morita@speee.jp>
2019-08-18 19:04:39 +0900

    いらないもの消した

--- index.php
+++ index.php
@@ -9,29 +9,6 @@
 <body>
 
 <!-- Head[Start] -->
-<header>
-  <nav class="navbar navbar-default">
-    <div class="container-fluid">
-    <div class="navbar-header"><a class="navbar-brand" href="select.php">データ一覧</a></div>
-    </div>
-  </nav>
-</header>
-<!-- Head[End] -->
-
-<!-- Main[Start] -->
-<!-- ここでinsert.phpにデータを送っている -->
-<form method="post" action="insert.php">
-  <div class="jumbotron">
-   <fieldset>
-    <legend>フリーアンケート</legend>
-    <!-- name="xxx" の部分に注目するようにしておいてください🤗 -->
-     <label>名前：<input type="text" name="name"></label><br>
-     <label>Email：<input type="text" name="email"></label><br>
-     <label><textArea name="naiyou" rows="4" cols="40"></textArea></label><br>
-     <input type="submit" value="送信">
-    </fieldset>
-  </div>
-</form>
 <!-- Main[End] -->
 
 
